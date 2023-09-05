d3c8dae8363fde62b75b7ad976cb78d63f0bc354
“laozz” <piganof@gmail.com>
2023-09-05 15:23:32 +0800

    Changes

--- src/test/java/com/example/mavendemo01/MavenDemo01ApplicationTests.java
+++ src/test/java/com/example/mavendemo01/MavenDemo01ApplicationTests.java
@@ -5,6 +5,8 @@ import com.example.mavendemo01.SQL.Article;
 import com.example.mavendemo01.SQL.ArticleMapper;
 import com.example.mavendemo01.SQL.Comment;
 import com.example.mavendemo01.SQL.CommentMapper;
+import com.example.mavendemo01.SQL.jpa.Discuss;
+import com.example.mavendemo01.SQL.jpa.DiscussRepository;
 import com.example.mavendemo01.ShujuFanhui.Person;
 import com.example.mavendemo01.ShujuFanhui.Student;
 import com.example.mavendemo01.ShujuFanhui.User;
@@ -18,6 +20,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -42,6 +46,9 @@ class MavenDemo01ApplicationTests {
     private CommentMapper commentMapper;
     @Autowired
     private ArticleMapper articleMapper;
+    @Autowired
+    private DiscussRepository repository;
+
 
     @Test
     void contextLoads() throws RuntimeException {
@@ -53,14 +60,14 @@ class MavenDemo01ApplicationTests {
 //        System.out.println(customProperties);
 //        System.out.println(dbConnector.config());
 
-
+//查询数据库字段
         //int id = 2;
-        Article article = articleMapper.findById(id);
-        System.out.println("查询数据库ID为" + id + "Comment表的数据----" + article);
-        System.out.println("-----------------------------------");
-        Comment comment = commentMapper.findById(id);
-        System.out.println("查询数据库ID为" + id + "的Comment表的数据----" + comment);
-        System.out.println("-----------------------------------");
+//        Article article = articleMapper.findById(id);
+//        System.out.println("查询数据库ID为" + id + "Comment表的数据----" + article);
+//        System.out.println("-----------------------------------");
+//        Comment comment = commentMapper.findById(id);
+//        System.out.println("查询数据库ID为" + id + "的Comment表的数据----" + comment);
+//        System.out.println("-----------------------------------");
 //
 //        //更新数据库字段
 //        comment.setaId(1);
@@ -70,16 +77,16 @@ class MavenDemo01ApplicationTests {
 //
 //        //插入数据库字段
 //        Comment comment1 = new Comment();
-//
 //        comment1.setAuthor("aihjuha");
 //        comment1.setContent("dad");
 //        commentMapper.insertComment(comment1);
 //        System.out.println(comment1);
 //        System.out.println("-----------------------------------");
 //
-//
 //        //删除数据库字段
 //        for (int i = 6; i < 30; i++)
 //            commentMapper.deleteComment(i);
+        List<Discuss> list = repository.findByAuthorNotNull();
+        System.out.println(list);
     }
 }
