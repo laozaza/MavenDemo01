d7c73dbc6c21764b76504ab10bd94338cfe5edc2
“laozz” <piganof@gmail.com>
2023-09-11 09:16:47 +0800

    Changes

--- src/main/java/com/example/mavendemo01/Thymeleaf/LoginController.java
+++ src/main/java/com/example/mavendemo01/Thymeleaf/LoginController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Calendar;
+
 @Controller
 public class LoginController {
     @GetMapping("/toLoginPage")
