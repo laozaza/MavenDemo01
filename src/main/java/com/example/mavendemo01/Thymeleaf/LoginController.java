7830fd58802f976c35f61e3e1e82ca38571ca5ed
“laozz” <piganof@gmail.com>
2023-09-11 09:13:16 +0800

    Changes

---
+++ src/main/java/com/example/mavendemo01/Thymeleaf/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.mavendemo01.Thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/toLoginPage")
+    public String toLoginPage(Model model){
+        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+        return "login";
+    }
+}
