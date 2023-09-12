ff69d9c4a7add9baedfab755707f293ff627b062
“laozz” <piganof@gmail.com>
2023-09-12 16:47:53 +0800

    未完成

--- src/main/java/com/example/mavendemo01/MVC/MyInterceptor.java
+++ src/main/java/com/example/mavendemo01/MVC/MyInterceptor.java
@@ -1,12 +1,18 @@
 package com.example.mavendemo01.MVC;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-public class MyInterceptor {
+/*
+* 自定义一个拦截器类
+* */
+@Configuration
+public class MyInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-                             Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //用户请求/admin开头路径,判断用户是否登录
         String uri = request.getRequestURI();
         Object loginUser = request.getSession().getAttribute("loginUser");
         if (uri.startsWith("/admin") && null == loginUser) {
@@ -14,6 +20,14 @@ public class MyInterceptor {
             return false;
         }
         return true;}
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+                           Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse
+            response, Object handler, @Nullable Exception ex) throws Exception {
+    }
 
 }
-
--- src/main/java/com/example/mavendemo01/MVC/MyMvcConfig.java
+++ src/main/java/com/example/mavendemo01/MVC/MyMvcConfig.java
@@ -1,12 +1,18 @@
-package com.example.mavendemo01.Thymeleaf;
+package com.example.mavendemo01.MVC;
 
 
+import com.example.mavendemo01.Thymeleaf.MyLocalResovel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/*
+实现WebMvcConfigurer接口,用于对MVC框架功能扩展
+*/
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
     //自定义的国际化组件
@@ -16,9 +22,11 @@ public class MyMvcConfig implements WebMvcConfigurer {
     }
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        //请求toLoginPage映射路径或者logi.html页面都会自动映射到login.html页面
         registry.addViewController("/toLoginPage").setViewName("login");
         registry.addViewController("/login.html").setViewName("login");
     }
+
 }
 
 
