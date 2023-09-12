9c169d57720fc0d90ebbf382564d8b1d838f36f9
“laozz” <piganof@gmail.com>
2023-09-12 14:23:47 +0800

    thymeleaf 1.0

--- src/main/java/com/example/mavendemo01/contorller/controller.java
+++ src/main/java/com/example/mavendemo01/contorller/controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
+    //定义HTTP GET请求的URL路径
     @GetMapping("/hlleo")
     public String hello(){
         return "hello";
